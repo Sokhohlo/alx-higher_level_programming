a11099c0f7d345cdf288f2fc78efb27a63c8035a
Sokhohlo <ssokhohlo@gmail.com>
2023-06-06 20:51:39 +0000

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
-"""Print all possible different combinations of two digits in ascending order.
-The two digits must be different - 01 and 10 are considered identical.
-"""
-for digit1 in range(0, 10):
-for digit2 in range(digit1 + 1, 10):
-if digit1 == 8 and digit2 == 9:
-print("{}{}".format(digit1, digit2))
-else:
-print("{}{}".format(digit1, digit2), end=", ")
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ", end="")
+print("")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 9-print_last_digit.py
 def print_last_digit(number):
-"""Print the last digit of a number and return it."""
-print(abs(number) % 10, end="")
-return (abs(number) % 10)
+    last_digit = number % 10 if number >= 0 else -number % 10
+    print(f"{last_digit}", end="")
+    return last_digit
